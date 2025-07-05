6b67b4e28e9560df31c1adf892d7450fb2031676
Zameerahmad01 <zameerahmad4877@gmail.com>
2025-07-05 09:49:08 +0500

    enhance Hero component with video playback, and improve Navbar animations.

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -3,11 +3,15 @@ import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/all";
 import { SplitText } from "gsap/all";
-import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useRef } from "react";
+import { useMediaQuery } from "react-responsive";
 
 const Hero = () => {
+  const videoRef = useRef(null);
+
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+  // Animation
   useGSAP(() => {
     gsap.registerPlugin(ScrollTrigger);
     const titleSplit = new SplitText(".title", {
@@ -24,7 +28,7 @@ const Hero = () => {
 
     gsap.from(titleSplit.chars, {
       opacity: 0,
-      y: 100,
+      y: 200,
       duration: 1.5,
       ease: "power3.out",
       stagger: 0.05,
@@ -79,54 +83,102 @@ const Hero = () => {
       },
       0
     );
+
+    const startValue = isMobile ? "top 50%" : "center 60%";
+    const endValue = isMobile ? "120% top" : "bottom top";
+
+    // Ensure videoRef is available and video is loaded before setting up timeline
+
+    const videoTimeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: "video", // Use the class of the video container
+        start: startValue,
+        end: endValue,
+        scrub: true,
+        pin: true,
+        markers: true,
+        onEnter: () => {
+          videoRef.current.play();
+        },
+        onEnterBack: () => {
+          videoRef.current.play();
+        },
+
+        onLeave: () => {
+          videoRef.current.pause();
+        },
+        onLeaveBack: () => {
+          videoRef.current.pause();
+        },
+      },
+    });
+
+    videoRef.current.onloadedmetadata = () => {
+      videoTimeline.to(videoRef.current, {
+        currentTime: videoRef.current.duration,
+      });
+    };
   }, []);
   return (
-    <section
-      className="relative z-10 pt-20 min-h-dvh w-full border border-transparent "
-      id="hero"
-    >
-      <h1 className="title  text-8xl md:text-[15vw] text-center leading-none font-modern-negra">
-        MOJITO
-      </h1>
-      <img
-        src="/images/cocktail-left-leaf.png"
-        alt="leaf"
-        className="left-leaf"
-      />
-      <img
-        src="/images/cocktail-right-leaf.png"
-        alt="leaf"
-        className="right-leaf"
-      />
+    <>
+      <section
+        className="relative z-10 pt-24 md:pt-20 min-h-dvh w-full border border-transparent "
+        id="hero"
+      >
+        <h1 className="title  text-8xl md:text-[15vw] text-center leading-none font-modern-negra">
+          MOJITO
+        </h1>
+        <img
+          src="/images/cocktail-left-leaf.png"
+          alt="leaf"
+          className="left-leaf"
+        />
+        <img
+          src="/images/cocktail-right-leaf.png"
+          alt="leaf"
+          className="right-leaf"
+        />
 
-      <div className="container mx-auto absolute left-1/2 -translate-x-1/2 lg:bottom-20 top-auto md:top-[30vh] flex justify-between items-end px-5">
-        <div className="content flex lg:flex-row flex-col w-full gap-10 justify-between items-center lg:items-end mx-auto">
-          <div className="space-y-3 hidden md:block">
-            <p className="paraTitle 2xl:text-start text-center">
-              Simple Ingredients, Bold Flavor
-            </p>
-            <p className="para font-modern-negra text-5xl text-yellow max-w-xl">
-              Sip the spirit <br /> of summer
-            </p>
-          </div>
+        <div className="container mx-auto absolute left-1/2 -translate-x-1/2 lg:bottom-20 top-auto md:top-[30vh] flex justify-between items-end px-5">
+          <div className="content flex lg:flex-row flex-col w-full gap-10 justify-between items-center lg:items-end mx-auto">
+            <div className="space-y-3 hidden md:block">
+              <p className="paraTitle 2xl:text-start text-center">
+                Simple Ingredients, Bold Flavor
+              </p>
+              <p className="para font-modern-negra text-5xl text-yellow max-w-xl">
+                Sip the spirit <br /> of summer
+              </p>
+            </div>
 
-          <div className="space-y-3 text-lg lg:max-w-2xs md:max-w-xs w-full">
-            <p className="para text-left text-base">
-              Every cocktail on our menu is a blend of premium ingredients,
-              creative flair, and timeless recipes — designed to delight your
-              senses.
-            </p>
+            <div className="space-y-3 text-lg lg:max-w-2xs md:max-w-xs w-full">
+              <p className="para md:text-left text-center text-base">
+                Every cocktail on our menu is a blend of premium ingredients,
+                creative flair, and timeless recipes — designed to delight your
+                senses.
+              </p>
 
-            <Link
-              href="#"
-              className=" font-semibold opacity-80 xl:text-start text-center hover:text-yellow"
-            >
-              <p className="paraLink">View cocktails</p>
-            </Link>
+              <Link
+                href="#"
+                className=" font-semibold opacity-80 xl:text-start text-center hover:text-yellow"
+              >
+                <p className="paraLink">View cocktails</p>
+              </Link>
+            </div>
           </div>
         </div>
+      </section>
+
+      <div className="w-full  md:h-[80%] h-1/2 absolute inset-0 bottom-0 left-0 md:object-contain object-bottom object-cover">
+        <video
+          ref={videoRef}
+          src="/videos/output.mp4"
+          playsInline
+          preload="auto"
+          muted
+          className="mx-auto"
+        />
       </div>
-    </section>
+    </>
   );
 };
 
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,12 +2,31 @@
 import { navLinks } from "@/constants";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
 const Navbar = () => {
   useGSAP(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    gsap
+      .timeline()
+      .from(".logo", {
+        y: 20,
+        opacity: 0,
+        duration: 0.5,
+        ease: "power1.inOut",
+      })
+      .from(".navLinks", {
+        y: 20,
+        opacity: 0,
+        duration: 0.5,
+        ease: "power1.inOut",
+        stagger: 0.05,
+      });
+
     const navTween = gsap.timeline({
       scrollTrigger: {
         trigger: "header",
@@ -32,7 +51,10 @@ const Navbar = () => {
     <header className="fixed z-50 w-full py-5 flex items-center ">
       <nav className="container mx-auto">
         <div className="flex flex-col md:flex-row items-center md:justify-between gap-4">
-          <Link href="#" className="flex items-center gap-2">
+          <Link
+            href="#"
+            className="logo flex items-center gap-2 overflow-hidden"
+          >
             <Image
               src="/images/logo.png"
               alt="logo"
@@ -44,12 +66,12 @@ const Navbar = () => {
               Cocktails
             </p>
           </Link>
-          <div className="flex items-center justify-center flex-wrap gap-7 lg:gap-12">
+          <div className="flex items-center justify-center flex-wrap gap-7 lg:gap-12 overflow-hidden">
             {navLinks.map((link, index) => (
               <Link
                 href={link.id}
                 key={index}
-                className="text-sm md:text-base text-nowrap font-semibold"
+                className="navLinks text-sm md:text-base text-nowrap font-semibold"
               >
                 {link.title}
               </Link>
